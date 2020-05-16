d2bb0a9e19623d51d817eadbc3ee6b61a44ed9b1
Manjot Sidhu <manjot.techie@gmail.com>
2020-05-16 11:51:02 +0530

    Index: Verify Auth token at every page.

--- main/views.py
+++ main/views.py
@@ -1,10 +1,29 @@
+import requests
 from django.shortcuts import render, redirect
 
+import daftar
 from main.models import LoginForm, SignupForm
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    if verify_token(request):
+        return redirect('/dashboard')
+    else:
+        return render(request, 'index.html', {})
+
+# TODO: Make this function global to all templates
+def verify_token(request):
+    if 'token' in request.session:
+        token = request.session['token']
+        hed = {'Authorization': 'Bearer ' + token}
+
+        r = requests.get(daftar.settings.DAFTAR_HOST + "/user", headers=hed)
+        if r.status_code == requests.codes.ok:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 def login(request):
